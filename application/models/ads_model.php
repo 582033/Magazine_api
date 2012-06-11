d0fdb5aaa6ec2bc91d3099062b03d93e944fbc5b
Meng Tong <mtong@eee168.com>
2012-06-11 10:39:59 +0800

    ads_model加position判断

--- application/models/ads_model.php
+++ application/models/ads_model.php
@@ -5,8 +5,12 @@ class Ads_Model extends mag_db {
 		 parent::__construct();
 	}
 
-    function _get_ads_links($items){    //获取广告内容{{{                                                                                                                          
-        $where = array('position' => $items['position']);                                                                                                                          
+    function _get_ads_links($items){    //获取广告内容{{{   
+		if (isset($items['position'])){                                                                                                                       
+			$where = array('position' => $items['position']);                                                                                                                          
+		}else{
+			$where = array();
+		}
         $return = $this->mag_db->rows(AD_TABLE, $where, $items['limit'], $items['start']);                                                                                         
         if ($return == array()) $return = null;                                                                                                                                    
         return $return;                                                                                                                                                            
