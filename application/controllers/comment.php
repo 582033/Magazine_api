44dd2794fe6b298e1d4607d54f32c3b9bccaaa61
Ping Yin <ping@eee168.com>
2012-07-17 17:57:03 +0800

    Fix return value and input format for comment.post api

--- application/controllers/comment.php
+++ application/controllers/comment.php
@@ -14,16 +14,25 @@ class Comment extends MY_Controller {
 			$now = new DateTime;
 			$date = $now->format("Y-m-d H:i:s");
 			$user_id = $this->check_session_model->check_session();
+			$comment_json = file_get_contents('php://input', 'r');
+			$comment = @json_decode($comment_json, TRUE);
+			if (!$comment) {
+				show_error_text(400, 'body not json');
+			}
+			if (empty($comment['content'])) {
+				show_error_text(400, 'Bad request, empty content param');
+			}
 			$com_data = array(
 				'type' => $type,
 				'object_id' => $magazineId,
-				'comment' => $this->input->post('comment'),
-				'parent_id' => $this->input->post('parent_id'),
+				'comment' => $comment['content'],
+				'parent_id' => element('parentId', $comment, 0),
 				'user_id' => $user_id,
 				'send_time' => date('Y-m-d H:i:s'),
 			);
 			$data = json_encode($com_data, true);
 			$this->User_comment_Model->add_comment($com_data);
+			show_error_text(201);
 		}
 		if($request_method == 'get'){
 			$limit = $this->input->get('limit') ? $this->input->get('limit') : '10';
