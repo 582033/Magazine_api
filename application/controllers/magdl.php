9e2e6f684a6a885b9abb89337dca77a28b95566d
Yan Jiang <yjiang@eee168.com>
2012-08-31 12:00:30 +0800

    下载数+1合并进下载api

--- application/controllers/magdl.php
+++ application/controllers/magdl.php
@@ -9,6 +9,10 @@
 		if(!in_array($filename, $filenames)){
 			show_error_text(404);
 		}else{
+			//下载数+1{{{
+			$this->load->model('mag_model');
+			$this->mag_model->incr_magazine($magazine_id, 'downloads');
+			//}}}
 			$read_mag_id = substr($magazine_id, 0, 3);
 			$filename = preg_replace('/^(mag)/', $magazine_id, $filename);
 	    	$url = $this->config->item('pub_host') . "/$read_mag_id/$magazine_id/dist/$filename";
